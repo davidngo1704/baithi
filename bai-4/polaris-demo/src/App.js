d031e1c2cb0f108206e4874f3349f525a729a4bc
davidngo1704 <108397620+davidngo1704@users.noreply.github.com>
2024-10-14 09:26:35 +0700

    sa

--- bai-4/polaris-demo/src/App.js
+++ bai-4/polaris-demo/src/App.js
@@ -17,8 +17,8 @@ function DiscountForm() {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [options, setOptions] = useState([
-    { title: 'Single', subTitle: "Standard price", label: "", quantity: 1, discountType: 'None', amount: '' },
-    { title: 'Duo', subTitle: "Save 10%", label: "Popular", quantity: 2, discountType: 'percent', amount: '10' },
+    { title: 'Single', subtitle: "Standard price", label: "", quantity: 1, discountType: 'None', amount: '' },
+    { title: 'Duo', subtitle: "Save 10%", label: "Popular", quantity: 2, discountType: 'percent', amount: '10' },
   ]);
   const [errors, setErrors] = useState({});
   const [preview, setPreview] = useState([]);
@@ -44,6 +44,12 @@ function DiscountForm() {
       if (option.title === '') {
         formErrors[`title_${index}`] = 'Title is required';
       }
+      if (option.subtitle === '') {
+        formErrors[`subtitle_${index}`] = 'Subtitle is required';
+      }
+      if (option.label === '') {
+        formErrors[`label_${index}`] = 'Label is required';
+      }
       if (option.quantity === '' || isNaN(option.quantity)) {
         formErrors[`quantity_${index}`] = 'Quantity must be a number';
       }
@@ -57,7 +63,7 @@ function DiscountForm() {
 
   const handleAddOption = () => {
     const lastQuantity = options[options.length - 1]?.quantity || 0;
-    const newOption = { title: '', quantity: lastQuantity + 1, discountType: 'None', amount: '' };
+    const newOption = { title: '', subtitle: "", label: "", quantity: lastQuantity + 1, discountType: 'None', amount: '' };
     setOptions([...options, newOption]);
   };
 
@@ -85,148 +91,195 @@ function DiscountForm() {
   return (
     <Page title="Create volume discount">
       <Layout>
-      <div style={{display: "flex", justifyContent: "center"}}>
-        {/* Form Section */}
-        <Layout.Section>
-          <FormLayout>
-           
-            <Card sectioned title="Campaign Details">
-            <h1>General</h1>
-              <TextField
-                label="Campaign"
-                value={campaignName}
-                onChange={(value) => setCampaignName(value)}
-                error={errors.campaignName}
-                placeholder='Volume discount #2'
-              />
-              <TextField
-                label="Title"
-                value={title}
-                onChange={(value) => setTitle(value)}
-                error={errors.title}
-                placeholder='buy more and save'
-              />
-              <TextField
-                label="Description"
-                value={description}
-                onChange={(value) => setDescription(value)}
-                error={errors.description}
-                placeholder='Apply for all products in store'
-              />
-            </Card>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {/* Form Section */}
+          <div style={{ width: "60%" }}>
+            <Layout.Section>
+              <FormLayout>
 
-            <Card sectioned title="Options">
-              {options.map((option, index) => (
-                <Card key={index} sectioned>
-                  <div style={{ position: 'relative' }}>
-                    {/* Nút delete ở góc trên bên phải */}
-                    <div style={{display: "flex", justifyContent: "space-between"}}>
-                      <h1 style={{
-                        "background-color": "#f3725b",
-                        "padding": "3px",
-                        color: "white"
-                      }}>Option {index + 1}</h1>
-                    <Button
-                      icon={DeleteIcon}
-                      onClick={() => handleRemoveOption(index)}
-                      plain
-                      style={{}}
-                      accessibilityLabel="Remove option"
-                    />
-                    </div>
-            
-                    <Layout>
-                      <Layout.Section oneThird>
-                        <TextField
-                          label="Title"
-                          value={option.title}
-                          onChange={(value) => handleFieldChange('title', value, index)}
-                          error={errors[`title_${index}`]}
-                        />
-                      </Layout.Section>
-                      <Layout.Section oneThird>
-                        <TextField
-                          label="Quantity"
-                          type="number"
-                          value={option.quantity}
-                          onChange={(value) => handleFieldChange('quantity', value, index)}
-                          error={errors[`quantity_${index}`]}
-                        />
-                      </Layout.Section>
-                      <Layout.Section oneThird>
-                        <Select
-                          label="Discount Type"
-                          options={discountTypes}
-                          value={option.discountType}
-                          onChange={(value) => handleFieldChange('discountType', value, index)}
-                        />
-                        {(option.discountType === 'percent' || option.discountType === 'fixed') && (
-                          <TextField
-                            label="Amount"
-                            suffix={option.discountType === 'percent' ? '%' : '$'}
-                            type="number"
-                            value={option.amount}
-                            onChange={(value) => handleFieldChange('amount', value, index)}
-                            error={errors[`amount_${index}`]}
-                          />
-                        )}
-                      </Layout.Section>
-                    </Layout>
-                    {/* Hiển thị lỗi */}
-                    {errors[`title_${index}`] && (
-                      <InlineError message={errors[`title_${index}`]} fieldID={`title_${index}`} />
-                    )}
-                    {errors[`quantity_${index}`] && (
-                      <InlineError message={errors[`quantity_${index}`]} fieldID={`quantity_${index}`} />
-                    )}
-                    {errors[`amount_${index}`] && (
-                      <InlineError message={errors[`amount_${index}`]} fieldID={`amount_${index}`} />
-                    )}
-                  </div>
+                <Card sectioned title="Campaign Details">
+                  <h1>General</h1>
+                  <TextField
+                    label="Campaign"
+                    value={campaignName}
+                    onChange={(value) => setCampaignName(value)}
+                    error={errors.campaignName}
+                    placeholder='Volume discount #2'
+                  />
+                  <TextField
+                    label="Title"
+                    value={title}
+                    onChange={(value) => setTitle(value)}
+                    error={errors.title}
+                    placeholder='buy more and save'
+                  />
+                  <TextField
+                    label="Description"
+                    value={description}
+                    onChange={(value) => setDescription(value)}
+                    error={errors.description}
+                    placeholder='Apply for all products in store'
+                  />
                 </Card>
-              ))}
 
-              <div>
+                <Card sectioned title="Options">
+                  {options.map((option, index) => (
+                    <Card key={index} sectioned>
+                      <div style={{ position: 'relative' }}>
+                        {/* Nút delete ở góc trên bên phải */}
+                        <div style={{ display: "flex", justifyContent: "space-between" }}>
+                          <h1 style={{
+                            "background-color": "#f3725b",
+                            "padding": "3px",
+                            color: "white"
+                          }}>Option {index + 1}</h1>
+                          <Button
+                            icon={DeleteIcon}
+                            onClick={() => handleRemoveOption(index)}
+                            plain
+                            style={{}}
+                            accessibilityLabel="Remove option"
+                          />
+                        </div>
 
-              </div>
-              <Button fullWidth id="custom-add-option-button" onClick={handleAddOption}>Add Option</Button>
-            </Card>
-              
-            <Button fullWidth primary id='custom-save-button' onClick={handleSave}>
-              Save
-            </Button>
-          </FormLayout>
-        </Layout.Section>
+                        <Layout>
+                          <div style={{ display: "flex", justifyContent: "center" }}>
+                            <div style={{ width: "33%" }}>
+                              <Layout.Section oneHalf>
+                                <TextField
+                                  label="Title"
+                                  value={option.title}
+                                  onChange={(value) => handleFieldChange('title', value, index)}
+                                  error={errors[`title_${index}`]}
+                                />
+                              </Layout.Section>
+                            </div>
+                            <div style={{ width: "33%" }}>
+                              <Layout.Section oneThird>
+                                <TextField
+                                  label="Subtitle"
+                                  value={option.subtitle}
+                                  onChange={(value) => handleFieldChange('subtitle', value, index)}
+                                  error={errors[`subTitle_${index}`]}
+                                />
+                              </Layout.Section>
+                            </div>
+                            <div style={{ width: "33%" }}>
+                              <Layout.Section oneThird>
+                                <TextField
+                                  label="Label"
+                                  value={option.label}
+                                  onChange={(value) => handleFieldChange('label', value, index)}
+                                  error={errors[`label_${index}`]}
+                                />
+                              </Layout.Section>
+                            </div>
+                          </div>
 
-        {/* Preview Section */}
-        <Layout.Section secondary>
-          <Card sectioned title="Preview Options">
-            <table style={{ width: '100%', borderCollapse: 'collapse' }}>
-              <thead>
-                <tr>
-                  <th style={{ border: '1px solid #ccc', padding: '8px' }}>Title</th>
-                  <th style={{ border: '1px solid #ccc', padding: '8px' }}>Quantity</th>
-                  <th style={{ border: '1px solid #ccc', padding: '8px' }}>Discount Type</th>
-                  <th style={{ border: '1px solid #ccc', padding: '8px' }}>Amount</th>
-                </tr>
-              </thead>
-              <tbody>
-                {preview.map((option, index) => (
-                  <tr key={index}>
-                    <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.title}</td>
-                    <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.quantity}</td>
-                    <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.discountType}</td>
-                    <td style={{ border: '1px solid #ccc', padding: '8px' }}>
-                      {option.discountType === 'percent' ? `${option.amount}%` : option.discountType === 'fixed' ? `$${option.amount}` : ''}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Card>
-        </Layout.Section>
+                          <div style={{ display: "flex", justifyContent: "center" }}>
+                            <div style={{ width: "33%" }}>
+                              <Layout.Section oneThird>
+                                <TextField
+                                  label="Quantity"
+                                  type="number"
+                                  value={option.quantity}
+                                  onChange={(value) => handleFieldChange('quantity', value, index)}
+                                  error={errors[`quantity_${index}`]}
+                                />
+                              </Layout.Section> </div>
+
+                            <div style={{ width: "33%" }}>
+                              <Layout.Section oneThird>
+                                <Select
+                                  label="Discount Type"
+                                  options={discountTypes}
+                                  value={option.discountType}
+                                  onChange={(value) => handleFieldChange('discountType', value, index)}
+                                />
+
+                              </Layout.Section>
+                            </div>
+
+
+                            {(option.discountType === 'percent' || option.discountType === 'fixed') && (
+                              <div style={{ width: "33%" }}>
+                                <Layout.Section oneThird>
+                                  <TextField
+                                    label="Amount"
+                                    suffix={option.discountType === 'percent' ? '%' : '$'}
+                                    type="number"
+                                    value={option.amount}
+                                    onChange={(value) => handleFieldChange('amount', value, index)}
+                                    error={errors[`amount_${index}`]}
+                                  />
+                                </Layout.Section>
+                              </div>
+                            )}
+
+
+                          </div>
+
+
+                        </Layout>
+                        {/* Hiển thị lỗi */}
+                        {errors[`title_${index}`] && (
+                          <InlineError message={errors[`title_${index}`]} fieldID={`title_${index}`} />
+                        )}
+                        {errors[`quantity_${index}`] && (
+                          <InlineError message={errors[`quantity_${index}`]} fieldID={`quantity_${index}`} />
+                        )}
+                        {errors[`amount_${index}`] && (
+                          <InlineError message={errors[`amount_${index}`]} fieldID={`amount_${index}`} />
+                        )}
+                      </div>
+                    </Card>
+                  ))}
+
+                  <div>
+
+                  </div>
+                  <Button fullWidth id="custom-add-option-button" onClick={handleAddOption}>Add Option</Button>
+                </Card>
+
+                <Button fullWidth primary id='custom-save-button' onClick={handleSave}>
+                  Save
+                </Button>
+              </FormLayout>
+            </Layout.Section>
+
+          </div>
+          <div style={{ width: "40%" }}>
+            {/* Preview Section */}
+            <Layout.Section secondary>
+              <Card sectioned title="Preview Options">
+                <table style={{ width: '100%', borderCollapse: 'collapse' }}>
+                  <thead>
+                    <tr>
+                      <th style={{ border: '1px solid #ccc', padding: '8px' }}>Title</th>
+                      <th style={{ border: '1px solid #ccc', padding: '8px' }}>Quantity</th>
+                      <th style={{ border: '1px solid #ccc', padding: '8px' }}>Discount Type</th>
+                      <th style={{ border: '1px solid #ccc', padding: '8px' }}>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {preview.map((option, index) => (
+                      <tr key={index}>
+                        <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.title}</td>
+                        <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.quantity}</td>
+                        <td style={{ border: '1px solid #ccc', padding: '8px' }}>{option.discountType}</td>
+                        <td style={{ border: '1px solid #ccc', padding: '8px' }}>
+                          {option.discountType === 'percent' ? `${option.amount}%` : option.discountType === 'fixed' ? `$${option.amount}` : ''}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </Card>
+            </Layout.Section>
+          </div>
+
         </div>
-   
+
       </Layout>
     </Page>
   );
